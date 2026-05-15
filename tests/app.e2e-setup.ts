37bc78d5f1181dbbb5f4c5ded4005095da005e45
Nikita <nikitatsarenko7@gmail.com>
2026-05-15 17:58:36 +0300

    tests: add app setup

---
+++ tests/app.e2e-setup.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+
+export async function createTestApp(): Promise<INestApplication> {
+    const moduleRef = await Test.createTestingModule({
+        imports: [AppModule],
+    }).compile();
+
+    const app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
+    app.setGlobalPrefix('v1', {
+        exclude: ['health'],
+    });
+    await app.init();
+
+    return app;
+}
